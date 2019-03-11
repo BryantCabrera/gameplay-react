f37a1584be5afa2119e3edbe4bd73dcf4465b2c3
Bryant <cabrera.bryant@gmail.com>
2019-03-11 12:19:48 -0700

    Adds Routes and Components to App.js.

--- src/App.js
+++ src/App.js
@@ -14,13 +14,52 @@ import Chat from './components/Chat/Chat';
 import Footer from './components/Footer/Footer';
 
 class App extends Component {
+  state = {
+    registerDisplay: 'none',
+    loginDisplay: 'none',
+    loggedUser: {}
+  }
+
+  toggleRegister = () => {
+    const display = this.state.registerDisplay === 'none' && this.state.loginDisplay === 'none' ? 'flex' : 'none';
+    this.setState({
+      registerDisplay: display
+    });
+  }
+
+  toggleLogin = () => {
+    const display = this.state.loginDisplay === 'none' && this.state.registerDisplay === 'none' ? 'flex' : 'none';
+    this.setState({
+      loginDisplay: display
+    });
+  }
+
+  loginUser = (user) => {
+    this.setState({
+      loggedUser: user
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <Header />
+        <Main />
+        <Register history={this.props.history} registerDisplay={this.state.registerDisplay} toggleRegister={this.toggleRegister} />
+        <Login history={this.props.history} loginDisplay={this.state.loginDisplay} toggleLogin={this.toggleLogin} loginUser={this.loginUser} />
+        <NavBar toggleRegister={this.toggleRegister} toggleLogin={this.toggleLogin} />
+        <Switch>
+          {/* <Route exact path="/register" component={() =>  <Register history={this.props.history} /> } /> */}
+          {/* <Route exact path="/login" component={() =>  <Login history={this.props.history} toggleLogin={this.toggleLogin} loginDisplay={this.state.loginDisplay} /> } /> */}
+          <Route exact path="/games" component={() => <Games history={this.props.history} />} />
+          <Route exact path="/users" component={() => <Users history={this.props.history} />} />
+          <Route exact path="/users/id" component={() => <UserProfile history={this.props.history} />} />
+        </Switch>
+        <Chat />
+        <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withRouter(App);
