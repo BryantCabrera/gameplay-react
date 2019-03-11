81e419c2855541ee9be8ab36676675168e33d876
Bryant <cabrera.bryant@gmail.com>
2019-03-11 12:26:02 -0700

    Creates Header in Header.js and adds faviconand GitHub icons to public.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { } from '@fortawesome/free-solid-svg-icons';
+// import logo from '../../static/imgs/logo.svg';
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <NavLink exact to="/" className="header__logo--link">
+                    <img className="header__logo" src="https://i.imgur.com/Y40XSUQ.png" alt="Game Play Logo" title="Game Play Logo"></img></NavLink>
+                <ul className="header__links">
+                    <li><img src="../../../public/github-brands.svg" alt="GitHub" title="GitHub"></img></li>
+                </ul>
+            </header>
+        )
+    }
+}
+
+export default withRouter(Header);
