8a329f48c43494577f69dfb9c79992f8d1a74a27
Bryant <cabrera.bryant@gmail.com>
2019-03-11 12:18:41 -0700

    Sets up imports in App.js.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
+import Main from './components/Main/Main';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import Users from './components/Users/Users';
+import UserProfile from './components/Users/UserProfile';
+import Games from './components/Games/Games';
+import Chat from './components/Chat/Chat';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
