c9a0ef0e42d23a0446b14ee37bfbb0ee30870c71
Bryant <cabrera.bryant@gmail.com>
2019-03-12 13:13:31 -0700

    Sets up TicTacToe.js component.

---
+++ src/components/Games/TicTacToe.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import '../../App.css';
+
+class TicTacToe extends Component {
+    
+}
+
+export default withRouter(Games);
